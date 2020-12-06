ea87478ced701b175b25e96955dac148bdb5253d
Marcus Luebke <marcusxluebke@gmail.com>
2020-12-05 16:57:23 -0800

    what a meme

---
+++ day4/puzzle2.fsx
@@ -0,0 +1,16 @@
+open System.Security.Cryptography
+open System.Text
+
+let md5 = MD5.Create()
+let hash (data:string) :string =
+   (StringBuilder(), md5.ComputeHash (Encoding.ASCII.GetBytes data))
+   ||> Array.fold (fun sb b -> sb.Append (b.ToString "x2"))
+   |> string
+
+let input = "ckczppom"
+let hashedHas5 (hshd:string) = hshd.[0..5] = "000000"
+
+let mutable num = 0
+while (input + string num) |> hash |> hashedHas5 |> not do
+   num <- num + 1
+printfn "%d" num
