6a0f81440434c707f9baca5f8e300659c49b96e8
Marcus Luebke <marcusxluebke@gmail.com>
2021-01-31 17:26:05 -0800

    struggling to relearn f#

--- day6/puzzle1.fsx
+++ day6/puzzle1.fsx
@@ -1,1 +1,8 @@
-let updateMap (map:bool[][]) (str:string) = 
+let indexed2 = Array.map Array.indexed >> Array.indexed
+let mapdex2 fn arr =
+   arr |> indexed2 |> Array.map (fun (i, arr1) -> arr1 |> Array.map (fun (j, b) -> fn (i,j) b))
+let parseString (str:string) :(bool -> bool) = 
+
+let updateMap (map:bool[][]) (str:string) = map |> mapdex2 (fun (i, j, b) -> )
+
+//Suffering
